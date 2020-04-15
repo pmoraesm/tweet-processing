45fb16fa944c5139e20118b91ebf10a8300d3075
Paulo Moraes <paulomoraes@Paulos-MacBook-Pro.local>
2020-04-15 21:40:09 +0200

    spark-submit docker image

---
+++ docker-images/count_hashtags/count_hashtags.py
@@ -0,0 +1,95 @@
+from pyspark.sql import *
+from pyspark.sql.types import *
+import pyspark.sql.functions as f
+
+
+def getSchema(file):
+    schema = spark.read \
+        .option("multiline", "true") \
+        .json(file) \
+        .schema
+    return schema
+
+
+def readKafkaStream(spark, schema, bootstrap, topic):
+    kafkaDf = spark.readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", bootstrap) \
+        .option("subscribe", topic) \
+        .load() \
+        .select(f.from_json(f.col("value").cast("string"),schema).alias("data")) \
+        .filter(f.col("data.lang").isin(['ja','ar','ko','th','ur','und','zh','in','tr','tl','hi']) == False)
+    return kafkaDf
+
+
+def extractDf(df):
+    expr = ">(.*?)<"
+    extractDf = df \
+        .withColumn("src", f.regexp_extract(f.col("data.source"), expr, 1)) \
+        .select(f.col("data.id").alias("twtid"),
+                f.col("data.timestamp_ms").alias("twttimestamp"),
+                f.col("data.lang").alias("twtlang"),
+                f.col("src").alias("twtsource"), 
+                f.explode("data.entities.hashtags").alias("hashtags")
+            )
+    return extractDf
+
+
+def flattenDf(df):
+    ht_schema  = StructType(
+            [
+                StructField('text', StringType(), True),
+                StructField('indices', StringType(), True)
+            ]
+        )
+    flatDf = df \
+        .withColumn("ht", f.from_json("hashtags", ht_schema)) \
+        .withColumn("twtdate", f.from_unixtime(f.col("twttimestamp").substr(1,10), format='yyyy-MM-dd')) \
+        .drop("hashtags") \
+        .select(f.col("twtid"),
+            f.col("twtdate"),
+            f.col("twtlang"),
+            f.col("twtsource"),
+            f.col("ht.text").alias("twthashtag")
+            )
+    return flatDf
+
+
+def aggregateDf(df):
+    aggDf = df \
+        .groupby('twtlang', 'twtdate', 'twtsource','twthashtag') \
+        .agg(f.count('twtid').alias('twtcount'))
+    return aggDf
+
+
+def writeCassandra(writeDf, epochId):
+    writeDf.write \
+        .format("org.apache.spark.sql.cassandra") \
+        .options(table="ht_count", keyspace="tweets_ks") \
+        .mode("append") \
+        .save()
+
+
+def runQuery(df):
+    query = df.writeStream \
+        .outputMode("update") \
+        .foreachBatch(writeCassandra) \
+        .start()
+    
+    query.awaitTermination()
+    query.stop()
+
+
+def main():
+    spark = SparkSession.builder.getOrCreate()
+    schema = getSchema("/home/app/sample_tweet.json")
+    kafkaDf = readKafkaStream(spark, schema, "tweets-kafka:9092", "tweets")
+    #prepDf = extractDf(kafkaDf)
+    #flatDf = flattenDf(extractDf(kafkaDf))
+    aggDf = aggregateDf(flattenDf(extractDf(kafkaDf)))
+    runQuery(aggDf)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ docker-images/count_hashtags/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+/usr/local/bin/spark-submit \
+--packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.5,datastax:spark-cassandra-connector:2.4.0-s_2.11 \
+--conf spark.cassandra.connection.host=tweets-db-cassandra \
+--conf spark.cassandra.auth.username=admin \
+--conf spark.cassandra.auth.password=tweets \
+count_hashtags.py
